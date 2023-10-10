e4978b67fb91cd27c4b17c21d245d67e5deeb026
Batuhan Şanlı <batuhansanli@gmail.com>
2023-10-10 13:25:38 +0300

    added module assert

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -1,11 +1,20 @@
 __all__ = ['ModuleType']
 
-from enum import Enum
+from enum import Enum, EnumMeta, unique
 from src.modules.text_input import TextInput
 from src.modules.text import Text
 
 
-class ModuleType(Enum):
+class EnumMetaAssert(EnumMeta):
+    def __getitem__(self, name):
+        try:
+            return super().__getitem__(name)
+        except (TypeError, KeyError) as error:
+            raise Exception(f'Module "{name}" not found') from error
+
+
+@unique
+class ModuleType(Enum, metaclass=EnumMetaAssert):
     text_input = TextInput
     text = Text
     # openai = 'openai'
