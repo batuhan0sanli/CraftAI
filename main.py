0ecf99436de4632d6969e2b5ff8b11f253495e4d
Batuhan Şanlı <batuhansanli@gmail.com>
2023-10-10 15:59:05 +0300

    removed hamburger menu

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ st.set_page_config(
     }
 )
 
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 yaml_path = st.sidebar.selectbox('Select applet', yaml_list)
 page_builder = PageBuilder(os.path.join('applets', yaml_path))
 page_builder.build()
