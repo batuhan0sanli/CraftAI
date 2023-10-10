62905db9201fd252c1b178341af3c6bb5c03ccd1
Batuhan Şanlı <batuhansanli@gmail.com>
2023-10-10 13:35:25 +0300

    added SubBaseSchema for nested schemas

--- src/base/base_schema.py
+++ src/base/base_schema.py
@@ -1,8 +1,16 @@
-from marshmallow import Schema, fields, validate, validates_schema, ValidationError, EXCLUDE
+from marshmallow import Schema, fields, EXCLUDE
+
+
+class SubBaseSchema(Schema):
+    """Sub base schema for nested data."""
+
+    class Meta:
+        unknown = EXCLUDE
 
 
 class BaseSchema(Schema):
     """Base schema if module returns a data."""
+
     class Meta:
         unknown = EXCLUDE
 
