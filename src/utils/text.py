20c3366339b9e59ec4aa96efe44b8bf5f79e078d
Batuhan Şanlı <batuhansanli@gmail.com>
2023-10-10 14:58:08 +0300

    added enum parse to text obj

--- src/utils/text.py
+++ src/utils/text.py
@@ -1,9 +1,16 @@
+from enum import Enum
+
+
 class TextObj(object):
     def __init__(self, text: str, variables: dict = None) -> None:
         self.text = text
         self.variables = variables or {}
 
     def __str__(self) -> str:
+        # Parse Enum values
+        if isinstance(self.text, Enum):
+            return self.text.value
+        # Parse variables
         return self.text.format(**self.variables)
 
     def __repr__(self) -> str:
